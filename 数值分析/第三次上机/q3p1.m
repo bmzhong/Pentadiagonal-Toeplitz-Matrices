bb8b0d7b2f34e89cdf6f1acdbae309c25a6a46a1
menglijinghua <508304482@qq.com>
2019-11-21 21:40:12 +0800

    all

--- 数值分析/第三次上机/q3p1.m
+++ 数值分析/第三次上机/q3p1.m
@@ -1,7 +1,12 @@
-func=inline('1/x','x');
-max=func(1);
-min=func(2);
+syms x y;
+y=1/x;
+f=matlabFunction(y);
+y1=diff(y);
+f1=matlabFunciton(y1);
+max=f(1);
+min=f(2);
 p0x=(max+min)/2;
 %零次最佳一致逼近多项式：
 fprintf('f(x)在[1,2]上的零次最佳一致逼近多项式为：%d\n',p0x);
-a1=(func(2)-func(1))/(b-a);
+a1=(f(2)-f(1))/(b-a);
+solve()
