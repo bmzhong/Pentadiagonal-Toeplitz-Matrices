eaedb98c0f392917d012586db223dc66540ce882
menglijinghua <508304482@qq.com>
2019-11-21 23:17:58 +0800

    all

--- 数值分析/第三次上机/q4.m
+++ 数值分析/第三次上机/q4.m
@@ -5,15 +5,11 @@ f=matlabFunction(y);
 y1=diff(y);
 f1=matlabFunction(y1);
 a=0;  b=pi/2;
-max=f(b);
-min=f(a);
-p0x=(max+min)/2;
-digits(4);
-fprintf('f(x)在[1,2]上的零次最佳一致逼近多项式为：%4.3d\n',p0x);
 a1=(f(b)-f(a))/(b-a);
 A=solve(y1==a1);
 x2=A(1);
 a0=(f(a)+f(x2))/2-a1*(a+x2)/2;
 y=a1*x+a0;
+digits(4)
 fprintf('f(x)在[1,2]上的一次最佳一致逼近多项式为:  ');
 disp(vpa(y));
