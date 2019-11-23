467a9e5dff4bc5208e1d3de938afef188af2f5b9
menglijinghua <508304482@qq.com>
2019-11-23 22:19:59 +0800

    all

--- 数值分析/第三次上机/p20.m
+++ 数值分析/第三次上机/p20.m
@@ -2,7 +2,7 @@
 x1=[19,25,31,38,44];
 y1=[19.0,32.3,49.0,73.3,97.8];
 f = inline('beta(1)+beta(2).*x.^2','beta','x');
-beta0=[0.5,0.5]';
+beta0=[0.5,0.5]'; 
 beta=nlinfit(x1,y1,f,beta0);
 syms x y;
 y=beta(1)+beta(2)*x^2;
