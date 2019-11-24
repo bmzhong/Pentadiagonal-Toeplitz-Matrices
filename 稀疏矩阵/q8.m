94b62d6aabbc445ea8a0131e0e339d9e99b24a81
menglijinghua <508304482@qq.com>
2019-11-24 10:59:43 +0800

    all

--- 稀疏矩阵/q8.m
+++ 稀疏矩阵/q8.m
@@ -1,14 +1,16 @@
 %232页第8题。直接运行此脚本文件即可得到结果。
-a=1;b=2;
 f=@(x)sqrt(x);
-d0=integral(f,1,2);
+d0=integral(f,0,1);
 f1=@(x)sqrt(x).*x;
-d1=integral(f1,1,2);
+d1=integral(f1,0,1);
 d=[d0;d1];
-H=[1,1./2;1,1./3];
+H=[1,1./2;1/2,1./3];
 A=inv(H)*d;
 syms x y;
 y=A(1)+A(2)*x;
 fprintf('f(x)的一次最佳平方逼近多项式为：P1(x)=');
 digits(5);
 disp(vpa(y));
+f=@(x)x;
+error=integral(f,0,1)-A(1)*d0-A(2)*d1;
+disp(error);
