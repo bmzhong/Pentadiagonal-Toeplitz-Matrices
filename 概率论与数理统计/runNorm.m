ccba816bc5d258981b5dafb0f67ade49ec49d83c
menglijinghua <508304482@qq.com>
2019-12-04 21:36:44 +0800

    all

--- 概率论与数理统计/runNorm.m
+++ 概率论与数理统计/runNorm.m
@@ -1,4 +1,6 @@
-x1=-10:0.1:10; sigma1=0; u1=2;     
-normDistribution(1,x1,sigma1,u1);
+% x1=-10:0.1:10; sigma1=0; u1=2;     
+% normDistribution(1,x1,sigma1,u1);
 % x1=-10:0.1:10; sigma1=1; u1=3;     
 % normDistribution(3,x1,sigma1,u1);
+x1=-100:0.1:100; sigma1=-4; u1=20;     
+normDistribution(5,x1,sigma1,u1);
