b5fa183efd546ac8bac2fa19323f5e5d622f6388
menglijinghua <508304482@qq.com>
2019-12-04 23:31:41 +0800

    all

---
+++ 概率论与数理统计/tdistribution/runT.m
@@ -0,0 +1,6 @@
+% a=1;b=2;x=-10:0.1:10;
+% tdistribution(1,a,b,x) 
+% a=-1;b=12;x=-10:0.1:10;
+% tdistribution(3,a,b,x) 
+a=3;b=32;x=-10:0.1:10;
+tdistribution(3,a,b,x) 
---
+++ 概率论与数理统计/tdistribution/tdistribution.m
@@ -0,0 +1,10 @@
+function t=tdistribution(n,a,b,x) 
+clc;
+y=tpdf(x-a,b); %1是中心，2 是自由度
+figure(n);
+plot(x,y)
+hold on;
+y=tcdf(x-a,b);
+figure(n+1);
+plot(x,y)
+hold on
